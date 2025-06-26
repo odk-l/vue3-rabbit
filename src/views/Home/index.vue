c3e06c7ed72999afc36c33962f84c2dd8619c0d9
odk-l <3355804218@qq.com>
2025-06-26 17:11:49 +0800

    Layout Pinia优化

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         Home
+        <div style="height: 1200px;"></div>
     </div>
 </template>
 
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,6 +1,6 @@
 <template>
-    //
-    <LayoutNav />
+    <LayoutFix />
+    <!-- <LayoutNav /> -->
     <LayoutHeader />
     <RouterView />
     <LayoutFooter />
@@ -10,6 +10,13 @@
 import LayoutFooter from './components/LayoutFooter.vue';
 import LayoutHeader from './components/LayoutHeader.vue';
 //import LayoutNav from './components/LayoutNav.vue';
+import LayoutFix from './components/LayoutFix.vue';
+//获取导航列表的action
+import { useCategoryStore } from '@/stores/category';
+import { onMounted } from 'vue';
+const categoryStore = useCategoryStore()
+
+onMounted(() => categoryStore.getCategory())
 </script>
 
 <style scoped></style>
