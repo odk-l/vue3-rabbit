a5f374532a456f4ff421f6a97a3f354c6384c978
odk-l <3355804218@qq.com>
2025-06-27 15:41:59 +0800

    一级分类路由配置

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -9,13 +9,13 @@ export const lazyPlugin = {
             mounted(el, binding) {
                 //el:指令绑定元素
                 //binding: bingding.value 指令等于号后面绑定的表达式的值 图片url
-                console.log(el, binding.value)
+                //console.log(el, binding.value)
                 const targetIsVisible = shallowRef(false)
                 const { stop } = useIntersectionObserver(
                     el,
                     ([entry], observerElement) => {
                         targetIsVisible.value = entry?.isIntersecting || false
-                        console.log(targetIsVisible.value)
+                        //console.log(targetIsVisible.value)
                         if (targetIsVisible.value) {
                             el.src = binding.value
                             stop()
